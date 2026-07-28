52d7d8a434d53643c22b340b5a82ed9eda2e33cb
Christolius <totosimamora@gmail.com>
2026-07-28 10:37:53 +0700

    objek.js

--- BuPasha/objek.js
+++ BuPasha/objek.js
@@ -4,3 +4,11 @@ const siswa = {
     hobi: "Makan"
 }
 console.log(`${siswa.nama}\n${siswa.kelas}\n${siswa.hobi}`)
+console.log(`${siswa['nama']}\n${siswa['kelas']}\n${siswa['hobi']}`)
+
+siswa.kelas = "XI RPL 3"
+siswa.citaCita = "Pilot"
+
+for (const key in siswa){
+    console.log(`${key}: ${siswa[key]}`)
+}
