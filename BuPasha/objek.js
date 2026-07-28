3332c8506df14778831c37819165cb8c71520e84
Christolius <totosimamora@gmail.com>
2026-07-28 10:37:39 +0700

    first commit

---
+++ BuPasha/objek.js
@@ -0,0 +1,6 @@
+const siswa = {
+    nama: "Budi",
+    kelas: "X RPL 3",
+    hobi: "Makan"
+}
+console.log(`${siswa.nama}\n${siswa.kelas}\n${siswa.hobi}`)
---
+++ BuPasha/var.js
@@ -0,0 +1,21 @@
+let skor = 0
+let nyawa = 3
+let level = 1
+const namaPemain = "Pemain 1"
+
+console.log(`skor: ${skor}\nnyawa: ${nyawa}\nlevel: ${level}`)
+
+//namaPemain = "Pemain 2" //ERROR
+
+const num = 1
+const str = "hallo"
+const bool = true
+const undef = undefined
+const nul = null
+
+console.log(typeof num, typeof str, typeof bool, typeof undef, typeof nul)
+
+const nama = "Christolius Simamora"
+const sekolah = "SMK Telkom Malang"
+
+console.log(`Halo nama saya ${nama}, saya bersekolah di ${sekolah}.`)
