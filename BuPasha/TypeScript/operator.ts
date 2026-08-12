877e3f74ff074b737911b9b5ae0848e0cd28c5ef
Christolius <totosimamora@gmail.com>
2026-08-12 08:38:50 +0700

    ujian html

---
+++ BuPasha/TypeScript/operator.ts
@@ -0,0 +1,18 @@
+let a1 = 20;
+let a2 = 3;
+
+let jum = a1+a2;
+let perkalian = a1*a2;
+let selisih = a1-a2;
+let pangkat = a1**a2
+let sisa = a1%a2;
+let bagi = a1/a2;
+
+console.log(`
+    Jumlah: ${jum}
+    Perkalian: ${perkalian}
+    Selisih: ${selisih}
+    Pangkat: ${pangkat}
+    Sisa Bagi: ${sisa}
+    Bagi: ${bagi}    
+`)
